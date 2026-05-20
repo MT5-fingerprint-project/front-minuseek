460e39e10cb36e3268c47d74d5711c04c540262f
adrienqua <777adrien@gmail.com>
2026-05-20 18:03:29 +0200

    init app with vite, ts, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
 
   return (
     <>
     <div>
-      <h1>Home</h1>
+
+      <h1 className="text-red-500">Home</h1>
+      <Button>Click</Button>
     </div>
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,4 +10,9 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
