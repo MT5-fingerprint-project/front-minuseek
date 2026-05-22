c755bae468ef785481adbfb948367daadaf731dc
adrienqua <777adrien@gmail.com>
2026-05-22 14:53:32 +0200

    feat: improve case card UI

--- src/components/investigationCase/InvestigationCaseCard.tsx
+++ src/components/investigationCase/InvestigationCaseCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { INVESTIGATION_CASE_STATUS_LABELS } from '@/constants/investigationCase/investigationCase.constants'
 import { Badge } from '../ui/badge'
 import { Link } from 'react-router-dom'
+import { Calendar, MapPin } from 'lucide-react'
 
 export default function InvestigationCaseCard({ investigationCase }: { investigationCase: InvestigationCase }) {
   return (
@@ -10,15 +11,18 @@ export default function InvestigationCaseCard({ investigationCase }: { investiga
       <Card key={investigationCase.id} className="h-full">
         <CardHeader>
           <Badge variant="secondary">{INVESTIGATION_CASE_STATUS_LABELS[investigationCase.status]}</Badge>
-          <CardTitle className="text-base">Affaire N°{investigationCase.caseNumber}</CardTitle>
+          <CardTitle className="text-lg font-semibold">Affaire N°{investigationCase.caseNumber}</CardTitle>
           <CardDescription>PV N°{investigationCase.pvNumber}</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-1">
-          <div className="text-xs text-muted-foreground">
-            Crée le {investigationCase.createdAt.toLocaleDateString('fr-FR')}
+          <div className="text-xs text-muted-foreground flex items-center gap-2">
+            <Calendar className="h-4 w-4" />
+            <span>Crée le {investigationCase.createdAt.toLocaleDateString('fr-FR')}</span>
           </div>
-          <div className="text-sm text-muted-foreground line-clamp-2">{investigationCase.description}</div>
-          <div className="text-sm text-muted-foreground line-clamp-2">{investigationCase.location}</div>
+          <div className="text-sm text-muted-foreground line-clamp-2 flex items-center gap-2">
+            <MapPin className="h-4 w-4" />
+            <span>{investigationCase.location}</span>
+          </div>
         </CardContent>
       </Card>
     </Link>
