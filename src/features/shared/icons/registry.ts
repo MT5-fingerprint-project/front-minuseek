6e50844c0df6e4ecdc89d8fe7773a4920b7918ca
Ayline Travers <99130296+AylineT@users.noreply.github.com>
2026-06-11 16:07:23 +0200

    feat: add all icons

--- src/features/shared/icons/registry.ts
+++ src/features/shared/icons/registry.ts
@@ -1,15 +1,121 @@
+import ArrowSmallLeft from './arrow-small-left.svg?react'
+import ArrowSmallRight from './arrow-small-right.svg?react'
+import Calendar from './calendar.svg?react'
+import CalendarEnd from './calendar-end.svg?react'
+import Check from './check.svg?react'
+import ChevronLeft from './chevron-left.svg?react'
+import ChevronRight from './chevron-right.svg?react'
+import Circle from './circle.svg?react'
+import CircleLine from './circle-line.svg?react'
+import Close from './close.svg?react'
+import CloseSmall from './close-small.svg?react'
+import Compare from './compare.svg?react'
+import Contrast from './contrast.svg?react'
+import DateStart from './date-start.svg?react'
+import FileExport from './file-export.svg?react'
+import FingerprintCheck from './fingerprint-check.svg?react'
+import Folder from './folder.svg?react'
 import Home from './home.svg?react'
-import Investigation from './investigation.svg?react'
+import Image from './image.svg?react'
+import Import from './import.svg?react'
+import ImportOff from './import-off.svg?react'
+import ImportPlus from './import-plus.svg?react'
 import Information from './information.svg?react'
-import Folder from "./folder.svg?react"
-import DateStart from "./date-start.svg?react"
+import InvertColors from './invert-colors.svg?react'
+import Investigation from './investigation.svg?react'
+import Layers from './layers.svg?react'
+import LayersOff from './layers-off.svg?react'
+import Location from './location.svg?react'
+import Luminosity from './luminosity.svg?react'
+import Menu from './menu.svg?react'
+import Mirror from './mirror.svg?react'
+import Number from './number.svg?react'
+import NumberOff from './number-off.svg?react'
+import Palette from './palette.svg?react'
+import Pen from './pen.svg?react'
+import PenTrace from './pen-trace.svg?react'
+import Person from './person.svg?react'
+import PersonCheck from './person-check.svg?react'
+import PersonGroup from './person-group.svg?react'
+import PersonProfile from './person-profile.svg?react'
+import Plus from './plus.svg?react'
+import PlusSmall from './plus-small.svg?react'
+import Redo from './redo.svg?react'
+import Rotate from './rotate.svg?react'
+import Search from './search.svg?react'
+import Settings from './settings.svg?react'
+import Show from './show.svg?react'
+import ShowOff from './show-off.svg?react'
+import SidebarFill from './sidebar-fill.svg?react'
+import SidebarOutline from './sidebar-outline.svg?react'
+import TextOutline from './text-outline.svg?react'
+import Trace from './trace.svg?react'
+import Undo from './undo.svg?react'
+import Verified from './verified.svg?react'
+import WindowOff from './window-off.svg?react'
+import WindowOn from './window-on.svg?react'
+import ZoomIn from './zoom-in.svg?react'
+import ZoomOut from './zoom-out.svg?react'
 
 export const icons = {
-  home: Home,
-  investigation: Investigation,
-  information: Information,
+  arrowSmallLeft: ArrowSmallLeft,
+  arrowSmallRight: ArrowSmallRight,
+  calendar: Calendar,
+  calendarEnd: CalendarEnd,
+  check: Check,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  circle: Circle,
+  circleLine: CircleLine,
+  close: Close,
+  closeSmall: CloseSmall,
+  compare: Compare,
+  contrast: Contrast,
+  dateStart: DateStart,
+  fileExport: FileExport,
+  fingerprintCheck: FingerprintCheck,
   folder: Folder,
-  dateStart: DateStart
+  home: Home,
+  image: Image,
+  import: Import,
+  importOff: ImportOff,
+  importPlus: ImportPlus,
+  information: Information,
+  invertColors: InvertColors,
+  investigation: Investigation,
+  layers: Layers,
+  layersOff: LayersOff,
+  location: Location,
+  luminosity: Luminosity,
+  menu: Menu,
+  mirror: Mirror,
+  number: Number,
+  numberOff: NumberOff,
+  palette: Palette,
+  pen: Pen,
+  penTrace: PenTrace,
+  person: Person,
+  personCheck: PersonCheck,
+  personGroup: PersonGroup,
+  personProfile: PersonProfile,
+  plus: Plus,
+  plusSmall: PlusSmall,
+  redo: Redo,
+  rotate: Rotate,
+  search: Search,
+  settings: Settings,
+  show: Show,
+  showOff: ShowOff,
+  sidebarFill: SidebarFill,
+  sidebarOutline: SidebarOutline,
+  textOutline: TextOutline,
+  trace: Trace,
+  undo: Undo,
+  verified: Verified,
+  windowOff: WindowOff,
+  windowOn: WindowOn,
+  zoomIn: ZoomIn,
+  zoomOut: ZoomOut,
 }
 
 export type IconName = keyof typeof icons
